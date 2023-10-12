e3fe4b2b4eb9a2f39fd01691e7b7263066903d4b
Maksym Cherniienko <h0nza4ok@gmail.com>
2023-10-12 19:34:49 +0300

    Add project files.

---
+++ Lab_04_7/Lab_04_7.cpp
@@ -0,0 +1,43 @@
+﻿#include <iostream>
+#include <iomanip>
+#include <cmath>
+using namespace std;
+int main()
+{
+	double xp, xk, x, dx, eps, a = 0, R = 0, S = 0;
+	int n = 0;
+	cout << "|x| < 1" << endl << endl;
+	cout << "xp = "; cin >> xp;
+	cout << "xk = "; cin >> xk;
+	cout << "dx = "; cin >> dx;
+	cout << "eps = "; cin >> eps;
+	cout << fixed;
+	cout << "-------------------------------------------------" << endl;
+	cout << "|" << setw(4) << "x" << "    | "
+		<< setw(16) << "log((1+x)/(1-x))" << " |"
+		<< setw(6) << "S" << "     |"
+		<< setw(4) << "n" << "   |"
+		<< endl;
+	cout << "-------------------------------------------------" << endl;
+	x = xp;
+	while (x <= xk)
+	{
+		n = 0; 
+		a = 2; 
+		S = a;
+		do {
+			n++;
+			R = pow(x,2*n+1) / (2 * n + 1);
+			a *= R;
+			S += a;
+		} while (abs(a) >= eps);
+		cout << "|" << setw(6) << setprecision(2) << x << "  |"
+			<< setw(13) << setprecision(5) << log((1+x)/(1-x)) << "     |"
+			<< setw(9) << setprecision(5) << S << "  |"
+			<< setw(4) << n << "   |"
+			<< endl;
+		x += dx;
+	}
+	cout << "-------------------------------------------------" << endl;
+	return 0;
+}
